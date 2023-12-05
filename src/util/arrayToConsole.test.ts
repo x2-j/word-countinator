a2a92ea0a259511c13fb7ebe53da5a9d73206f8b
John Nerush <john@johndevelops.io>
2023-12-05 04:08:48 +0000

    Resolving issue with jest not using new module aliases

---
+++ src/util/arrayToConsole.test.ts
@@ -0,0 +1,13 @@
+import arrayToConsole from './arrayToConsole'
+
+describe('arrayToConsole', () => {
+  it('should output the given array to the console as rows of "word: count"', () => {
+    const arr = [['word', 1], ['word', 2], ['word', 3]]
+    const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {})
+    arrayToConsole(arr)
+    expect(consoleSpy).toHaveBeenCalledWith('word: 1')
+    expect(consoleSpy).toHaveBeenCalledWith('word: 2')
+    expect(consoleSpy).toHaveBeenCalledWith('word: 3')
+    consoleSpy.mockRestore()
+  })
+})
