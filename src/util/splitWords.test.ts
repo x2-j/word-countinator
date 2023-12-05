187c2ebdd7eaea26a2cee770e717c5fc2b6f3545
John Nerush <john@johndevelops.io>
2023-12-05 21:47:07 +0000

    Flesh out more tests

--- src/util/splitWords.test.ts
+++ src/util/splitWords.test.ts
@@ -19,4 +19,22 @@ describe('splitWords', () => {
       three: 1,
     })
   })
+  it('should split a string into words and ignore case', () => {
+    const str = 'one TWO THREE two'
+    const store = splitWords(str)
+    expect(store).toEqual({
+      one: 1,
+      two: 2,
+      three: 1,
+    })
+  })
+  it('should split a string into words and ignore punctuation', () => {
+    const str = 'one two, three   two'
+    const store = splitWords(str)
+    expect(store).toEqual({
+      one: 1,
+      two: 2,
+      three: 1,
+    })
+  })
 })
