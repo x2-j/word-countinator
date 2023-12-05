74633af4ed24f92392ab39f410d9e759cb717507
John Nerush <john@johndevelops.io>
2023-12-05 02:21:55 +0000

    Refactored readFile

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
 import { StoreObject, StoreArray } from '@interface/store'
 
-import readFile from './util/readFile'
+import { readFile, getFileNameFromArgs } from './util/readFile'
 import splitWords from './util/splitWords'
 import sortArray, { sortArrayByWord } from './util/sortArray'
-
-// Get the third argument from the command line as filename
-const filename: string = process.argv[2]
-// If no filename is provided, log an error and exit the process
-if (!filename || filename.length === 0) {
-  console.error('Please provide a filename "node-ts src/index.ts <filename>"')
-  process.exit(1)
-}
 
 // Start a store to cache each word and it's count
 let store: StoreObject = {}
 
 // Get contents of file as string
-const file: string = readFile(filename)
+const fileName: string = getFileNameFromArgs()
+const file: string = readFile(fileName)
+
+
+
 
 store = splitWords(file)
 
--- src/util/readFile.ts
+++ src/util/readFile.ts
@@ -2,6 +2,8 @@ import { readFileSync } from 'fs'
 
 /**
  * Reads the contents of a file and returns it as a string. 
+ * 
+ * @remarks
  * If the file does not exist, the process will exit with exit code 1
  * 
  * @param filename - The path to the file to be read.
@@ -27,13 +29,24 @@ export function readFile (filename: string): string {
 }
 
 
+/**
+ * Retrieves the filename from the command line arguments.
+ * 
+ * @remarks
+ * If no filename is provided or the filename does not have a .txt extension,
+ * the process will exit with exit code 9 (Invalid Argument).
+ * 
+ * @returns The filename as a string, or void if an error occurred.
+ */
 export function getFileNameFromArgs (): string {
-  // Get the third argument from the command line as filename
-  const filename: string = process.argv[2]
-  
-  if (!filename) { // Remove filename.length as it is redundant here
+  const filename: string = process.argv[2] || ''
+  if (filename === '') {
     console.error('Please provide a filename "node-ts src/index.ts <filename>"')
-    process.exit(1)
+    process.exit(9) // Exit Code 9: Invalid Argument
+  }
+  if (!filename.endsWith('.txt')) {
+    console.error('Please provide a filename with the .txt extension')
+    process.exit(9) // Exit Code 9: Invalid Argument
   }
   return filename
 }
